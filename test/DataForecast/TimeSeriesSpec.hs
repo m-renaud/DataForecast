706a175744a9fc261ec60fdd6e9dae5b08b52036
Matt Renaud <mrenaud92@gmail.com>
2017-01-16 13:28:32 -0800

    Add tests for 'fromParts'.

--- test/DataForecast/TimeSeriesSpec.hs
+++ test/DataForecast/TimeSeriesSpec.hs
@@ -9,9 +9,11 @@ import DataForecast.TimeSeries
 spec :: Spec
 spec = do
     rawSpec
+    fromPartsSpec
     defaultSummarySpec
     summaryWithTotalSpec
     setSdTotalSpec
+    setSdMeanSpec
 
 
 -- | Pin down the 'TimeSeries' type to avoid ambiguous instance errors.
@@ -21,11 +23,23 @@ rawDay = raw
 
 rawSpec :: Spec
 rawSpec =
-    describe "when raw constructor used" $ do
+    describe "when 'raw' constructor used" $ do
     it "summary data total is equal to 'raw' argument" $
         (sdtotal . getSD $ rawDay 10) `shouldBe` Just 10
     it "summary data mean is 'Nothing'" $
         (sdmean . getSD $ rawDay 10) `shouldBe` Nothing
+
+
+fromPartsSpec :: Spec
+fromPartsSpec =
+    describe "when 'fromParts' constructor used" $ do
+    context "with empty subparts list" $ do
+        it "has 'defaultSummary' data" $
+            (getSD $ (fromParts [] :: TimeSeries '[ 'Year, 'Quarter ]))
+             `shouldBe` defaultSummary
+        it "has empty subpartrs" $
+            (getSub $ (fromParts [] :: TimeSeries '[ 'Year, 'Quarter ]))
+            `shouldBe` Subparts (Just [])
 
 
 defaultSummarySpec :: Spec
@@ -58,5 +72,16 @@ setSdTotalSpec =
         it "sdtotal is overwritten" $
             (sdtotal . setSdTotal 20 . summaryWithTotal $ 10)
             `shouldBe` Just 20
-            
-    
+        it "sdmean is still Nothing" $
+            (sdmean . setSdTotal 20 . summaryWithTotal $ 10)
+            `shouldBe` Nothing
+
+
+setSdMeanSpec :: Spec
+setSdMeanSpec =
+    describe "when setSdMean called" $ do
+    context "on default 'SummaryData'" $ do
+        it "sdmean is Just" $
+            sdmean (setSdMean 10 defaultSummary) `shouldBe` Just 10
+        it  "sdtotal is still Nothing" $
+            sdtotal (setSdMean 10 defaultSummary) `shouldBe` Nothing
